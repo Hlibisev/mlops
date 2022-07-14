fc986f1bbd36e9b62126099d8497f17b00daa200
Hlibisev <hlibisev@gmail.com>
2022-07-14 09:06:08 +0300

    Resolve

--- run.py
+++ run.py
@@ -3,9 +3,5 @@ import numpy as np
 
 if __name__ == "__main__":
     print(np.random.rand(10))
-<<<<<<< HEAD
     print(np.random.randint(1))
     print(np.random.randint(1))
-=======
-    print(np.random.randint(10))
->>>>>>> master
